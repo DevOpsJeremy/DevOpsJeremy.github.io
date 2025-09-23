ff314433aa002176ca64f54cff8a4303efa90935
DevOps Jeremy <DevOpsJeremy@gmail.com>
2025-09-22 23:55:09 -0700

    Adjust css

--- hooks/macros.py
+++ hooks/macros.py
@@ -9,7 +9,7 @@ DOCS_ROOT = 'docs'
 DOCS_PATH = join(getcwd(), DOCS_ROOT)
 
 class Img:
-    def __init__(self, path, alt='', date_val='', site_uri=''):
+    def __init__(self, path, alt='', date_val='', site_uri='', class_name='photo'):
         self.site_uri = site_uri
         self.path = path
         if date_val == '' or date_val is None:
@@ -17,6 +17,7 @@ class Img:
         else:
             self.date = date_val if isinstance(date_val, date) else parser.parse(date_val)
         self.alt = alt if alt is not None else ''
+        self.class_name = class_name
     
     def to_url(self, path):
         if self.site_uri:
@@ -24,7 +25,7 @@ class Img:
         return path
     
     def to_string(self):
-        return f'<img class="photo-preview" src="{self.to_url(self.path)}" alt="{self.alt}" />'
+        return f'<div class="{self.class_name}"><img class="photo-preview" src="{self.to_url(self.path)}" alt="{self.alt}" /></div>'
     
     def __repr__(self):
         return f'{{path: {self.path}, alt: {self.alt}, date: {self.date}}}'
